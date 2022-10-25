120046cc34dcf3b12374b9bc40ccf362f2c25c2a
bobby1072 <75732572+bobby1072@users.noreply.github.com>
2022-10-25 12:05:53 +0100

    FishDeal product scrape complete

--- src/FishStoreClassLib/AnglingDirectClass.ts
+++ src/FishStoreClassLib/AnglingDirectClass.ts
@@ -1,11 +1,9 @@
 import BasicProductScrapeClass from "./BasicProductScrapeClass";
-import { IFishDealProduct } from "./FishDealClass";
-
 export class AnglingDirect extends BasicProductScrapeClass {
-  public Url: string = "https://www.anglingdirect.co.uk/";
+  public Url: string = "https://www.anglingdirect.co.uk";
   public async scrapeResults(searchTerm: string): Promise<void> {
-    const anglingDirPage = this.getInfoReq(
-      `${this.Url}catalogsearch/result/?q=${searchTerm}`
+    const anglingDirPage = await this.getInfoReq(
+      `${this.Url}/catalogsearch/result/?q=${searchTerm}`
     );
   }
 }
--- src/FishStoreClassLib/FishDealClass.ts
+++ src/FishStoreClassLib/FishDealClass.ts
@@ -7,26 +7,47 @@ export interface IFishDealProduct {
   name: string;
   position: number;
   price: number;
+  store: string;
+  baseLink: string;
+  imageSrc: string;
+  productLink: string;
 }
 export class FishDeal extends BasicProductScrapeClass {
-  public Url: string = "https://fishdeal.co.uk/";
+  public Url: string = "https://fishdeal.co.uk";
   public async scrapeResults(searchTerm: string): Promise<IFishDealProduct[]> {
     const searchedPage = await this.getInfoReq(
       `${this.Url}/search?q=${searchTerm}`
     );
     const $ = cheerio.load(searchedPage);
     const allItems = $("div.SC_DealsBlock-item");
-    let tempList: any[] = [];
+    const tempDataList: any[] = [];
+    const tempImgList: any[] = [];
+    allItems.find("div > div > a > span > img").each(function (index, element) {
+      const imgData = $(element).attr("data-src");
+      const imgAlt = $(element).attr("alt");
+      const imgJson = { src: imgData, alt: imgAlt };
+      tempImgList.push(imgJson);
+    });
     allItems.find("div > div > a").each(function (index, element) {
       const dealData = $(element).attr("data-gtm-payload");
+      const itemLink = $(element).attr("href");
       const parsedDealData = dealData && JSON.parse(dealData);
+      parsedDealData.ecommerce.click.products[0].productLink = itemLink;
       parsedDealData &&
         parsedDealData.ecommerce.click.products[0] &&
-        tempList.push(parsedDealData.ecommerce.click.products[0]);
+        tempDataList.push(parsedDealData.ecommerce.click.products[0]);
     });
-    const finalItemArray: IFishDealProduct[] = tempList.map((element) => {
+    const finalItemArray: IFishDealProduct[] = tempDataList.map((element) => {
       const newPrice: number | boolean = element.price && Number(element.price);
       newPrice && (element.price = newPrice);
+      element.imageSrc = `${this.Url}${
+        tempImgList.find((imgElement) => {
+          return imgElement.alt === element.name;
+        }).src
+      }`;
+      element.store = "Fish Deal";
+      element.baseLink = this.Url;
+      element.productLink = `${this.Url}${element.productLink}`;
       return element;
     });
     return finalItemArray;
