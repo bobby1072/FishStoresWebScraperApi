02788fce4a95cdbfc9710a23ad8e2b2d95c308c6
bobby1072 <75732572+bobby1072@users.noreply.github.com>
2022-10-25 18:22:38 +0100

    SportFishWorking Needs cleanup

--- src/FishStoreClassLib/AnglingDirectClass.ts
+++ src/FishStoreClassLib/AnglingDirectClass.ts
@@ -1,9 +1,64 @@
 import BasicProductScrapeClass from "./BasicProductScrapeClass";
-export class AnglingDirect extends BasicProductScrapeClass {
-  public Url: string = "https://www.anglingdirect.co.uk";
+import cheerio from "cheerio";
+interface ISportFishProd {
+  Name: string;
+  Link: string;
+  ImageSrc: string;
+  ImageAlt: string;
+  Price: number;
+}
+export class SportFish extends BasicProductScrapeClass {
+  public Url: string = "https://www.sportfish.co.uk";
   public async scrapeResults(searchTerm: string): Promise<void> {
     const anglingDirPage = await this.getInfoReq(
-      `${this.Url}/catalogsearch/result/?q=${searchTerm}`
+      `${this.Url}/catalogsearch/result/?q=+${searchTerm}`
     );
+    const $ = cheerio.load(anglingDirPage);
+    const productLinkAndNameList: any[] = [];
+    const allProductLinks = $("a.product-item-link");
+    allProductLinks.each((index, element) => {
+      const productLink = $(element).attr("href");
+      const productName = $(element).text();
+      productLink &&
+        productName &&
+        productLinkAndNameList.push({ Name: productName, Link: productLink });
+    });
+    const allProductImgs = $("img.product-image-photo");
+    const productImgSrcAndAlt: any[] = [];
+    allProductImgs.each((index, element) => {
+      const productImgSrc = $(element).attr("src");
+      const productImgAlt = $(element).attr("alt");
+      productImgSrc &&
+        productImgAlt &&
+        productImgSrcAndAlt.push({
+          ImageSrc: productImgSrc,
+          ImageAlt: productImgAlt,
+        });
+    });
+    const allProductPrices = $("span.price-wrapper");
+    const productPriceList: any[] = [];
+    allProductPrices.each((index, element) => {
+      const productPrice = $(element).attr("data-price-amount");
+      try {
+        productPrice && productPriceList.push(Number(productPrice));
+      } catch (e) {}
+    });
+    const finalItemArray: ISportFishProd[] = productLinkAndNameList.map(
+      (element, index) => {
+        const ImageSrcAlt = productImgSrcAndAlt.find((imgElement) => {
+          return element.Name === imgElement.ImageAlt;
+        });
+        ImageSrcAlt &&
+          (element.ImageAlt = ImageSrcAlt.ImageAlt) &&
+          (element.ImageSrc = ImageSrcAlt.ImageSrc);
+        let prodPrice = false;
+        try {
+          prodPrice = productPriceList[index];
+        } catch (e) {}
+        prodPrice && (element.Price = prodPrice);
+        return element;
+      }
+    );
+    console.log(finalItemArray);
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
+import { SportFish } from "./FishStoreClassLib/AnglingDirectClass";
 import { FishDeal } from "./FishStoreClassLib/FishDealClass";
 const main = async (): Promise<void> => {
-  const fishDeal = new FishDeal();
-  const fishDealItems = await fishDeal.scrapeResults("shark shad");
-  console.log(fishDealItems);
+  //const fishDeal = new FishDeal();
+  //const fishDealItems = await fishDeal.scrapeResults("shark shad");
+  //console.log(fishDealItems);
+  const anglingDirect = new SportFish();
+  await anglingDirect.scrapeResults("spinner");
 };
 main();
