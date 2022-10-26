7564615afaaef9cb29a60db2c6b8c60f7b5221ae
bobby1072 <75732572+bobby1072@users.noreply.github.com>
2022-10-26 17:06:46 +0100

    Api routes finished for now

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,64 @@
-const cors = require("cors");
+import cors from "cors";
 import AllItemShops from "./FishStoreClassLib/AllShopsClass";
 import express, { Application, Request, Response } from "express";
 import bodyParser from "body-parser";
 import ICommonFishProduct from "./FishStoreClassLib/ICommonStoreItemData";
-const main = async (): Promise<void> => {
-  /*const fishDeal = new FishDeal();
-  const fishDealItems = await fishDeal.scrapeResults("shark shad");
-  console.log(fishDealItems);
-  const anglingDirect = new SportFish();
-  console.log(await anglingDirect.scrapeResults("spinner"));
-  */
-  const allItemObj = new AllItemShops();
-  console.log(await allItemObj.getAllItems("sal"));
-};
+import { FishDeal } from "./FishStoreClassLib/FishDealClass";
+import { SportFish } from "./FishStoreClassLib/SportFishClass";
 const app: Application = express();
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+app.get(
+  "/sportfishproductsearch/",
+  async (req: Request, res: Response): Promise<void> => {
+    try {
+      const prodSearchName = req.query.searchterm;
+      if (
+        typeof prodSearchName === "string" &&
+        /^[A-Za-z\s]*$/.test(prodSearchName)
+      ) {
+        const sportFishObj: SportFish = new SportFish();
+        const sportFishItems: ICommonFishProduct[] =
+          await sportFishObj.scrapeResults(prodSearchName);
+        res.status(200);
+        res.json(sportFishItems);
+      } else {
+        res.status(400);
+        res.send("Possibly inncorrect URL argument given");
+      }
+    } catch (error) {
+      res.status(500);
+      res.send("Internal server error occured");
+    }
+  }
+);
+
+app.get(
+  "/fishdealproductsearch",
+  async (req: Request, res: Response): Promise<void> => {
+    try {
+      const prodSearchName = req.query.searchterm;
+      if (
+        typeof prodSearchName === "string" &&
+        /^[A-Za-z\s]*$/.test(prodSearchName)
+      ) {
+        const fishDealObj: FishDeal = new FishDeal();
+        const fishDealItems: ICommonFishProduct[] =
+          await fishDealObj.scrapeResults(prodSearchName);
+        res.status(200);
+        res.json(fishDealItems);
+      } else {
+        res.status(400);
+        res.send("Possibly inncorrect URL argument given");
+      }
+    } catch (error) {
+      res.status(500);
+      res.send("Internal server error occured");
+    }
+  }
+);
 
 app.get(
   "/allproductsearch/",
